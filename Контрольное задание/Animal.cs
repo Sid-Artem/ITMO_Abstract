41123180b05b643c4930ff79f1e3488ab31ea11e
Сидоров Артем <sidoroffartemii@gmail.com>
2024-08-18 12:20:27 +0300

    Добавьте файлы проекта.

---
+++ Контрольное задание/Animal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Контрольное_задание
+{
+    abstract class Animal
+    {
+        abstract public string Name { get; set; }
+        public Animal(string name) { Name = name; }
+
+         public abstract void Say();
+         public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }      
+    }
+    class Cat : Animal
+    {
+        string name;
+
+        public override string Name 
+        { get => name;
+          set => name=value; 
+        }
+        public Cat (string name)
+        :base(name) { }
+        
+        public override void Say()
+        {
+           Console.WriteLine("Мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        string name;
+
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name)
+        : base(name) { }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
---
+++ Контрольное задание/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Контрольное_задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cat Cat=new Cat("Мурка");
+            Dog Dog = new Dog("Норд");
+            Cat.ShowInfo();
+            Dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
